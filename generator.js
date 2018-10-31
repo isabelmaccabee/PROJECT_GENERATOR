01c74b915b11bddbaec76c4ae02850cfca01a664
isabelmaccabee <isabel.maccabee@gmail.com>
2018-10-31 15:31:28 +0000

    MADE IT MAKE SOME FILES AND DIRECTORS yes

--- generator.js
+++ generator.js
@@ -1,7 +1,38 @@
+// #!/usr/bin/env node
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 const curr_dir = process.cwd(); // WHERE THE USER IS ATM AND WHERE NEW FOLDER WILL BE
+console.log(curr_dir);
+
+const createDirectoryContents = (templatePath, projectName) => {
+  fs.readdir(templatePath, function(err, files) {
+    files.forEach(file => {
+      const originalFilePath = `${templatePath}/${file}`;
+      fs.stat(originalFilePath, (err, stat) => {
+        if (stat.isFile()) {
+          fs.readFile(originalFilePath, "utf8", (err, contents) => {
+            const writePath = `${curr_dir}/${projectName}/${file}`;
+            fs.writeFile(writePath, contents, "utf8", err => {
+              if (err) console.error("Failed oops: " + err);
+              else console.log("You made a file!!!");
+            });
+          });
+        } else if (stat.isDirectory()) {
+          //file here refers to directory <-- spec basically
+          fs.mkdir(`${curr_dir}/${projectName}/${file}`, err => {
+            if (err) console.error("Failed oh no: " + err);
+            else
+              createDirectoryContents(
+                `${templatePath}/${file}`,
+                `${projectName}/${file}`
+              );
+          });
+        }
+      });
+    });
+  });
+};
 
 const questions = [
   {
@@ -18,8 +49,7 @@ const questions = [
 
 inquirer.prompt(questions).then(answers => {
   const projectName = answers.project_name;
-  const templatePath = `${__dirname}/template_folder`; // HOW TO GET TO OUR TEMPLATES FROM WHERE THE USER IS ATM
-  fs.mkdir(`${curr_dir}/${projectName}`, function(err) {
-    if (err) console.log(err);
-  });
+  const templatePath = `${__dirname}/template_folder`;
+  fs.mkdir(`${curr_dir}/${projectName}`, function() {});
+  createDirectoryContents(templatePath, projectName);
 });
